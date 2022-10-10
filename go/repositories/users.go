7804e82f312185e58c255605ef4add831394acd6
adhxabre <abeldustin06@gmail.com>
2022-10-10 17:11:10 +0700

    Creating Mux Microservices

---
+++ go/repositories/users.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+	"dumbmerch/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	FindUsers() ([]models.User, error)
+	GetUser(ID int) (models.User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func RepositoryUser(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindUsers() ([]models.User, error) {
+	var users []models.User
+	err := r.db.Find(&users).Error
+
+	return users, err
+}
+
+func (r *repository) GetUser(ID int) (models.User, error) {
+	var user models.User
+	err := r.db.First(&user, ID).Error
+
+	return user, err
+}
